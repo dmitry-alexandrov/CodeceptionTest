515b01460fa07b2111b0a416c591518ef37bece6
Gambit <efin2012@yandex.ru>
2024-06-16 01:38:53 +0300

    Fix docker entrypoint

--- docker/php/root/docker-entrypoint.sh
+++ docker/php/root/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/www/src && composer self-update && composer install
+cd /var/www/src && composer self-update && composer install || 'echo composer install command Е*ЕТ МОЗК'
 
 supervisord -n -c /etc/supervisor/supervisord.conf &
 
