592f5ff43578c2d78f93ff812948dd0badbf8ebc
Gambit <efin2012@yandex.ru>
2024-06-28 12:16:53 +0300

    FIxes

--- docker/php/root/docker-entrypoint.sh
+++ docker/php/root/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /var/www/src && composer self-update && composer install || 'echo composer install command Е*ЕТ МОЗК'
 
-php-fpm${PHP_VERSION} -y /etc/php/${PHP_VERSION}/fpm/php-fpm.conf -R -F
+php-fpm8.3 -R -F
